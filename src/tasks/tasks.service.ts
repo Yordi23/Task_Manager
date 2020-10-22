3a8512a68460f9083ec600601391e1a7fe1b5b54
Yordi Ogando <yordiogando@gmail.com>
2020-10-22 17:55:27 -0400

    Added createTask functionality

--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -1,5 +1,8 @@
 import { Injectable } from '@nestjs/common';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
+import { v1 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+import { title } from 'process';
 
 @Injectable()
 export class TasksService {
@@ -7,5 +10,19 @@ export class TasksService {
 
   getAllTasks() {
     return this.tasks;
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
+    const task: Task = {
+      id: uuid(),
+      title,
+      description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+    return task;
   }
 }
