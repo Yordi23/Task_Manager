03767ec84d5a3a5a708b0943aaf65e211865a0d9
Yordi Ogando <yordiogando@gmail.com>
2020-10-23 17:41:54 -0400

    Implemented validation on createTask using validationPipe and class-validator package

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -7,6 +7,8 @@ import {
   Patch,
   Post,
   Query,
+  UsePipes,
+  ValidationPipe,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
@@ -33,6 +35,7 @@ export class TasksController {
   }
 
   @Post()
+  @UsePipes(ValidationPipe)
   createTask(
     // @Body('title') title: string,
     // @Body('description') description: string,
