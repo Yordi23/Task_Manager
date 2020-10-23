c0da00344426b15d7d5f0ca6540e1926ca4e61dd
Yordi Ogando <yordiogando@gmail.com>
2020-10-22 20:42:45 -0400

    Implemented getTaskById, deleteTask, and updateTaskStatus endpoints

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,16 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { TasksService } from './tasks.service';
-import { Task } from './task.model';
+import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 
 @Controller('tasks')
 export class TasksController {
@@ -12,6 +21,11 @@ export class TasksController {
     return this.tasksService.getAllTasks();
   }
 
+  @Get('/:id')
+  getTaskById(@Param('id') id: string): Task {
+    return this.tasksService.getTaskById(id);
+  }
+
   @Post()
   createTask(
     // @Body('title') title: string,
@@ -20,5 +34,28 @@ export class TasksController {
   ): Task {
     // return this.tasksService.createTask(title, description);
     return this.tasksService.createTask(createTaskDto);
+  }
+
+  @Delete('/:id')
+  deleteTask(@Param('id') id: string): void {
+    this.tasksService.deleteTask(id);
+  }
+
+  //My Solution
+  // @Patch('/:id/status')
+  // updateTaskStatus(
+  //   @Param('id') id: string,
+  //   @Body() updateTaskStatusDto: UpdateTaskStatusDto,
+  // ): Task {
+  //   return this.tasksService.updateTaskStatus(id, updateTaskStatusDto);
+  // }
+
+  //Course Solution
+  @Patch('/:id/status')
+  updateTaskStatus(
+    @Param('id') id: string,
+    @Body('status') status: TaskStatus,
+  ): Task {
+    return this.tasksService.updateTaskStatus(id, status);
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -3,6 +3,7 @@ import { Task, TaskStatus } from './task.model';
 import { v1 as uuid } from 'uuid';
 import { CreateTaskDto } from './dto/create-task.dto';
 import { title } from 'process';
+import { UpdateTaskStatusDto } from './dto/update-task-status.dto';
 
 @Injectable()
 export class TasksService {
@@ -10,6 +11,10 @@ export class TasksService {
 
   getAllTasks() {
     return this.tasks;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find(task => task.id === id);
   }
 
   createTask(createTaskDto: CreateTaskDto): Task {
@@ -23,6 +28,34 @@ export class TasksService {
     };
 
     this.tasks.push(task);
+    return task;
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter(task => task.id !== id);
+  }
+
+  //My Solution
+  //   updateTaskStatus(id: string, updateTaskStatusDto: UpdateTaskStatusDto): Task {
+  //     const { status } = updateTaskStatusDto;
+  //     let updatedTask;
+
+  //     this.tasks = this.tasks.map(task => {
+  //       if (task.id === id) {
+  //         task.status = status;
+  //         updatedTask = task;
+  //         return task;
+  //       }
+  //       return task;
+  //     });
+
+  //     return updatedTask;
+  //   }
+
+  //Course Solution
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const task = this.getTaskById(id);
+    task.status = status;
     return task;
   }
 }
