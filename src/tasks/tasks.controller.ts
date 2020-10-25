ab3b122369c9ad40e758f17e9a6c005c19f0a552
Yordi Ogando <yordiogando@gmail.com>
2020-10-25 19:57:36 -0400

    Code cleaning

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -11,15 +11,6 @@ import { TaskStatus } from './task-status.enum';
 export class TasksController {
     constructor(private tasksService: TasksService) { }
 
-    //   @Get()
-    //   getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Task[] {
-    //     if (Object.keys(filterDto).length) {
-    //       return this.tasksService.getTasksWithFilters(filterDto);
-    //     } else {
-    //       return this.tasksService.getAllTasks();
-    //     }
-    //   }
-
     @Get()
     getTasks(@Query(ValidationPipe) filterDto: GetTasksFilterDto): Promise<Task[]> {
         return this.tasksService.getTasks(filterDto);
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -13,30 +13,6 @@ export class TasksService {
     @InjectRepository(TaskRepository)
     private taskRepository: TaskRepository
   ) { }
-  // getAllTasks() {
-  //   return this.tasks;
-  // }
-  // getTasksWithFilters(filterDto: GetTasksFilterDto): Task[] {
-  //   const { status, search } = filterDto;
-  //   let tasks = this.getAllTasks();
-  //   if (status) {
-  //     tasks = tasks.filter(task => task.status === status);
-  //   }
-  //   if (search) {
-  //     tasks = tasks.filter(
-  //       task =>
-  //         task.title.includes(search) || task.description.includes(search),
-  //     );
-  //   }
-  //   return tasks;
-  // }
-  // getTaskById(id: string): Task {
-  //   const found = this.tasks.find(task => task.id === id);
-  //   if (!found) {
-  //     throw new NotFoundException(); //You can pass a custom message through parameters
-  //   }
-  //   return found;
-  // }
 
   async getTasks(filterDto: GetTasksFilterDto): Promise<Task[]> {
     return this.taskRepository.getTasks(filterDto)
@@ -51,26 +27,9 @@ export class TasksService {
     return found;
   }
 
-  // createTask(createTaskDto: CreateTaskDto): Task {
-  //   const { title, description } = createTaskDto;
-  //   const task: Task = {
-  //     id: uuid(),
-  //     title,
-  //     description,
-  //     status: TaskStatus.OPEN,
-  //   };
-  //   this.tasks.push(task);
-  //   return task;
-  // }
-
   async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
     return this.taskRepository.createTask(createTaskDto);
   }
-
-  // deleteTask(id: string): void {
-  //   const found = this.getTaskById(id);
-  //   this.tasks = this.tasks.filter(task => task.id !== id);
-  // }
 
   //Delete Solution with repository.remove()
   // async deleteTask(id: number): Promise<void> {
@@ -86,27 +45,6 @@ export class TasksService {
       throw new NotFoundException(); //You can pass a custom message through parameters
     }
   }
-
-  // //My Solution
-  // //   updateTaskStatus(id: string, updateTaskStatusDto: UpdateTaskStatusDto): Task {
-  // //     const { status } = updateTaskStatusDto;
-  // //     let updatedTask;
-  // //     this.tasks = this.tasks.map(task => {
-  // //       if (task.id === id) {
-  // //         task.status = status;
-  // //         updatedTask = task;
-  // //         return task;
-  // //       }
-  // //       return task;
-  // //     });
-  // //     return updatedTask;
-  // //   }
-  // //Course Solution
-  // updateTaskStatus(id: string, status: TaskStatus): Task {
-  //   const task = this.getTaskById(id);
-  //   task.status = status;
-  //   return task;
-  // }
 
   async updateTaskStatus(id: number, status: TaskStatus): Promise<Task> {
     const task = await this.getTaskById(id);
